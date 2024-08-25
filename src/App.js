68b5a1fb66b0624b388a36b4dbb177c645cfe2bf
dimaarion <dimaarion@gmail.com>
2024-08-25 03:55:11 +0300

    https://github.com/dimaarion/samurai.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber"
+import { Sky, PointerLockControls, KeyboardControls } from "@react-three/drei"
+import { Physics } from "@react-three/rapier"
+import { Ground } from "./Ground"
+import { Player } from "./Player"
+import { Cube, Cubes } from "./Cube"
 
-function App() {
+// The original was made by Maksim Ivanow: https://www.youtube.com/watch?v=Lc2JvBXMesY&t=124s
+// This demo needs pointer-lock, that works only if you open it in a new window
+// Controls: WASD + left click
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "w", "W"] },
+        { name: "backward", keys: ["ArrowDown", "s", "S"] },
+        { name: "left", keys: ["ArrowLeft", "a", "A"] },
+        { name: "right", keys: ["ArrowRight", "d", "D"] },
+        { name: "jump", keys: ["Space"] },
+      ]}>
+      <Canvas shadows camera={{ fov: 45 }}>
+        <Sky sunPosition={[100, 20, 100]} />
+        <ambientLight intensity={0.3} />
+        <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
+        <Physics gravity={[0, -30, 0]}>
+          <Ground />
+          <Player />
+          <Cube position={[0, 0.5, -10]} />
+          <Cubes />
+        </Physics>
+        <PointerLockControls />
+      </Canvas>
+    </KeyboardControls>
+  )
 }
-
-export default App;
