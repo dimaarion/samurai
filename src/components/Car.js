174bce5380d1ab75994e880c508a4eb6b07598e3
dimaarion <dimaarion@gmail.com>
2024-12-01 22:59:46 +0200

    dimaarion/samurai

--- src/components/Car.js
+++ src/components/Car.js
@@ -12,16 +12,14 @@ const directionVector = new Vector3();
 export default function Car(props) {
     const [, get] = useKeyboardControls();
     const {nodes, materials, animations} = useGLTF('./asset/model/VintageRacingCar.glb');
-    const {bodyWheel, actions} = useAnimations(animations)
-    const orbitControlRef = useRef();
-
 
     const WheelJoint = ({
                             body,
                             wheel,
                             bodyAnchor,
                             wheelAnchor,
-                            rotationAxis
+                            rotationAxis,
+        index
                         }) => {
         const joint = useRevoluteJoint(body, wheel, [
             bodyAnchor,
@@ -35,42 +33,18 @@ export default function Car(props) {
                 body.current?.wakeUp()
             }
 
-            const position = body.current?.translation();
-            const angularVelocity = body.current?.angvel()
-            const angularVelocityWheel = wheel.current?.angvel()
-            const rotation = wheel.current?.rotation()
-            const rotationBody = body.current?.rotation()
             if (joint.current) {
-                joint.current.configureMotorVelocity(forward ? -15 : backward ? 15 : 0, 10);
+                joint.current.configureMotorVelocity(forward ? -25 : backward ? 25 : 0, 10);
             }
-               body.current?.setAngvel({
-               x: angularVelocity.x, // Сохраняем скорость по X
-             y: leftward ? 1 : rightward ? -1 : 0, // Обновляем скорость по Y
-              z: angularVelocity.z  // Сохраняем скорость по Z
-               });
-            //  bodyWheel.current.position.x = position.x
-            //   bodyWheel.current.position.y = position.y + 1
-            //   bodyWheel.current.position.z = position.z
-
-
-            //  bodyWheel.current.rotation.z -= angularVelocity.y * delta / 2.028
-            //  wheelM.current.rotation.z = rotation.z
-            //  wheelM.current.rotation.x = rotation.x
-            // wheelM.current.rotation.y = rotation.y
-            // wheelM.current.rotation.y += 0.1
-
 
         });
 
         return null;
     };
 
-
     const bodyRef = useRef(null);
     //  const bodyWheel = useRef();
     const wheelPositions = [
-        [1.55, -1, -2.7],
-        [-1.75, -1, -2.7],
         [1.4, -1, 2.2],
         [-1.4, -1, 2.2],
 
@@ -85,47 +59,61 @@ export default function Car(props) {
     const wheelLeft = useRef();
 
 
-    useEffect(() => {
 
 
-        console.log(nodes)
-    }, [])
+    const platformWheelJoint = useRevoluteJoint(bodyRef,wheelRight,[
+        [0,-1.2,-2.8],
+        [0,0,0],
+        [1,0,0]
+    ])
+    const platformWheelLeftJoint = useRevoluteJoint(bodyRef,wheelLeft,[
+        [0,-1.2,-2.8],
+        [0,0,0],
+        [1,0,0]
+    ])
+
+
     useFrame((state, delta, frame) => {
         const {forward, backward, leftward, rightward, jump} = get();
         if (forward || backward || leftward || rightward) {
             wheelLeft.current?.wakeUp()
         }
-
+        if (platformWheelJoint.current) {
+            platformWheelJoint.current.configureMotorVelocity(leftward ? 30 : rightward ? -30: 0, 10);
+            platformWheelLeftJoint.current.configureMotorVelocity(leftward ? -30 : rightward ? 30 : 0, 10);
+        }
 
     })
 
     return (
         <group scale={2}>
 
-            <RigidBody colliders="cuboid" mass={100} ref={bodyRef} type="dynamic">
+            <RigidBody colliders="cuboid" ref={bodyRef} type="dynamic">
                 <mesh geometry={nodes.Body.geometry} material-color={"blue"} material={materials["car.002"]}/>
             </RigidBody>
 
-
+            <RigidBody   colliders="hull" ref={wheelRight} type="dynamic">
+                <mesh position={[-1,0,0]}  scale={[1.8, 1, 1]} geometry={nodes.front_right_wheel.geometry}
+                      material-color={"black"}
+                      material={materials["car.002"]}/>
+            </RigidBody>
+            <RigidBody  colliders="hull" ref={wheelLeft} type="dynamic">
+                <mesh position={[1,0,0]} scale={[1.8, 1, 1]} geometry={nodes.front_left_wheel.geometry}
+                      material-color={"black"}
+                      material={materials["car.002"]}/>
+            </RigidBody>
             {wheelPositions.map((wheelPosition, index) => (
                 <RigidBody
                     position={wheelPosition}
                     colliders="hull"
                     type="dynamic"
-
                     key={index}
                     ref={wheelRefs.current[index]}
 
                 >
-                    {index === 0 ?
-                        <mesh scale={[2, 1, 1]} geometry={nodes.front_left_wheel.geometry} material-color={"black"}
-                              material={materials["car.002"]}/> :
-                        index === 1 ?
-                            <mesh scale={[2, 1, 1]} geometry={nodes.front_right_wheel.geometry} material-color={"black"}
-                                  material={materials["car.002"]}/> :
-                            index === 2 ? <mesh scale={[1.4, 1, 1]} geometry={nodes.rear_right_wheel.geometry}
+                    {index === 0 ? <mesh scale={[1.4, 1, 1]} geometry={nodes.rear_right_wheel.geometry}
                                                 material-color={"black"}
-                                                material={materials["car.002"]}/> : index === 3 ?
+                                                material={materials["car.002"]}/> : index === 1 ?
                                 <mesh scale={[1.4, 1, 1]} geometry={nodes.rear_left_wheel.geometry}
                                       material-color={"black"}
                                       material={materials["car.002"]}/> : ""}
@@ -140,6 +128,7 @@ export default function Car(props) {
                     bodyAnchor={wheelPosition}
                     wheelAnchor={[0, 0, 0]}
                     rotationAxis={[1, 0, 0]}
+                    index={index}
                 />
             ))}
 
